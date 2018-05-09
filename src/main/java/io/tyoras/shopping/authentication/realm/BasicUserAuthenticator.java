8c8bd828ef54fff15285a0f6aae4e2f64e2e7fd5
tyoras <tyoras@gmail.com>
2018-05-10 01:23:59 +0200

    upgrade java 10

--- src/main/java/io/tyoras/shopping/authentication/realm/BasicUserAuthenticator.java
+++ src/main/java/io/tyoras/shopping/authentication/realm/BasicUserAuthenticator.java
@@ -1,40 +1,38 @@
 package io.tyoras.shopping.authentication.realm;
 
-import java.util.Optional;
-
-import javax.inject.Inject;
-import javax.inject.Singleton;
-
-import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.basic.BasicCredentials;
 import io.tyoras.shopping.user.SecuredUser;
 import io.tyoras.shopping.user.repository.SecuredUserRepository;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.Optional;
+
 @Singleton
 public class BasicUserAuthenticator implements Authenticator<BasicCredentials, BasicUserPrincipal> {
-	
+
 	private final SecuredUserRepository userRepository;
-	
+
 	@Inject
 	public BasicUserAuthenticator(SecuredUserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
 
     @Override
-    public Optional<BasicUserPrincipal> authenticate(BasicCredentials credentials) throws AuthenticationException {
+    public Optional<BasicUserPrincipal> authenticate(BasicCredentials credentials) {
     	String userEmail = extractUserEmailFromCredentials(credentials);
 		SecuredUser foundUser = userRepository.getByEmail(userEmail);
 		if (foundUser == null) {
 			return Optional.empty();
 		}
 		String hashedPassword = userRepository.hashPassword(credentials.getPassword(), foundUser.getSalt());
-    	return Optional.ofNullable(foundUser.getPassword().equals(hashedPassword) ? new BasicUserPrincipal(foundUser) : null);        
+    	return Optional.ofNullable(foundUser.getPassword().equals(hashedPassword) ? new BasicUserPrincipal(foundUser) : null);
 	}
-	
-    
+
+
     private String extractUserEmailFromCredentials(BasicCredentials credentials) {
 		return credentials.getUsername();
 	}
-    
+
 }
